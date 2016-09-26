ce1fdccf42caae86235ef5f32b0d31232bec5c64
jbishop-art <jonathan.bishop.art@gmail.com>
2016-09-26 12:41:15 -0600

    Lighting_update
    Added basic lighting to scene and flames to torches.

---
+++ Assets/Scripts/TorchLightFlicker.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TorchLightFlicker : MonoBehaviour {
+
+    private Light thisLight;
+    private Color originalColor;
+    private float timePassed;
+    private float changeValue;
+
+
+	// Use this for initialization
+	void Start () {
+
+        thisLight = this.GetComponent<Light>();
+
+        if (thisLight != null)
+        {
+            originalColor = thisLight.color;
+        }
+	    else
+        {
+            enabled = false;
+            return;
+        }
+
+        changeValue = 0;
+        timePassed = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        timePassed = Time.time;
+        timePassed = timePassed - Mathf.Floor(timePassed);
+
+        thisLight.color = originalColor * CalculateChange();
+	
+	}
+
+
+
+    private float CalculateChange()
+    {
+        changeValue = -Mathf.Sin((timePassed * (Random.Range(0.01f, 0.25f)) * Mathf.PI)) * 0.05f + 0.75f;
+        return changeValue;
+    }
+}
