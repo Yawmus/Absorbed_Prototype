8ec582c638adc55e6983b92f8dfa62d26545f0c9
Sam <smorris383@gmail.com>
2016-09-27 13:29:17 -0600

    Lighting Baked, Looks Better.

--- Assets/Scripts/MoveOnComplete.cs
+++ Assets/Scripts/MoveOnComplete.cs
@@ -16,14 +16,16 @@ public class MoveOnComplete : MonoBehaviour {
 		Debug.Log (enabled); //It always equals true, never false. FIGURE THIS OUT
 
 		if (enabled) {
-			if (transform.position.x < 20) {
-				transform.Translate (speed * Time.deltaTime, 0, 0); 	
-			}
-		} else {
-			if(transform.position.x > startingPos.x)
-				transform.Translate (-speed * Time.deltaTime, 0, 0); 
-				//transform.position = Vector3.MoveTowards (transform.position, startingPos, speed * Time.deltaTime); 
+			gameObject.SetActive(false); 
 		}
+//			if (transform.position.z < -11) {
+//				transform.Translate (speed * Time.deltaTime, 0, 0); 	
+//			}
+//		} else {
+//			if(transform.position.x > startingPos.x)
+//				transform.Translate (-speed * Time.deltaTime, 0, 0); 
+//				//transform.position = Vector3.MoveTowards (transform.position, startingPos, speed * Time.deltaTime); 
+//		}
 	}
 
 }
