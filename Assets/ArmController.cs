c73f6b4e17c61c9d7d14c96159d62817536aa374
jbishop-art <jonathan.bishop.art@gmail.com>
2016-09-27 18:54:27 -0600

    Animations attached to acitons.

---
+++ Assets/ArmController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArmController : MonoBehaviour
+{
+
+    Animator anim;
+    int jumpHash = Animator.StringToHash("jump");
+
+
+	// Use this for initialization
+	void Start ()
+    {
+
+        anim = GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        float move = Input.GetAxis("Vertical");
+        anim.SetFloat("speed", move);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger(jumpHash);
+        }
+	
+	}
+}
