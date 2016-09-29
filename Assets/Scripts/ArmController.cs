728f5809c902a537cc120ecc16eb93bbd19c5d10
jbishop-art <jonathan.bishop.art@gmail.com>
2016-09-28 21:43:24 -0600

    animation fixes:  holding orb and not holding orb work correclty.

--- Assets/Scripts/ArmController.cs
+++ Assets/Scripts/ArmController.cs
@@ -6,8 +6,11 @@ public class ArmController : MonoBehaviour
 
     Animator anim;
     int jumpHash = Animator.StringToHash("jump");
-    int holding_orbHash = Animator.StringToHash("holding_orb");
-    float distance = 10.0f;
+    int holdingHash = Animator.StringToHash("holding_orb");
+    int notholdingHash = Animator.StringToHash("not_holding_orb");
+    float distance_speed = 10.0f;
+    float distance_flip = 10.0f;
+    float distance_swap = 10.0f;
     public GameObject righthand;
     public GameObject speedorb;
     public GameObject fliporb;
@@ -20,33 +23,76 @@ public class ArmController : MonoBehaviour
     {
 
         anim = GetComponent<Animator>();
-        righthand = GameObject.Find("Player/arms/locator_right_arm/R_ForeArm/R_Hand/R_FingerBase");
-        speedorb = GameObject.Find("Orbs/Speed Orb");
-        fliporb = GameObject.Find("Orbs/Flip Orb");
-        swaporb = GameObject.Find("Orbs/Swap Orb");
+        righthand = GameObject.Find("Player/arms/locator_right_arm/R_ForeArm/R_Hand");
+        speedorb = GameObject.Find("Orbs/Speed orb/Point light");
+        fliporb = GameObject.Find("Orbs/Flip orb/Point light");
+        swaporb = GameObject.Find("Orbs/Swap orb/Point light");
+        //anim.SetTrigger(notholdingHash);
         
 
 
     }
-	
-	// Update is called once per frame
-	void Update ()
-    {
 
+    // Update is called once per frame
+    void Update()
+    {
         float move = Input.GetAxis("Vertical");
         anim.SetFloat("speed", move);
 
-        if(Input.GetKeyDown(KeyCode.Space))
+        // Check for Spacebar press.
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             anim.SetTrigger(jumpHash);
         }
 
-        distance = Vector3.Distance(righthand.transform.position, speedorb.transform.position);
-        if (distance<10)
+
+        //Check between distance of speedorb and palm of hand.  Hold or not old animation.
+        distance_speed = Vector3.Distance(righthand.transform.position, speedorb.transform.position);
+        distance_flip = Vector3.Distance(righthand.transform.position, fliporb.transform.position);
+        distance_swap = Vector3.Distance(righthand.transform.position, swaporb.transform.position);
+        Debug.Log("Distance between speed orb and hand is " + distance_speed); //prints to console distance between hand and orb
+
+        //holding or not holding SPEED ORB and driving corresponding animation.
+        if (distance_speed < .099f)
         {
-            anim.SetTrigger(holding_orbHash);
+            anim.ResetTrigger(notholdingHash);
+            anim.SetTrigger(holdingHash);
+        }
+        else if (distance_flip < .099f)
+        {
+            anim.ResetTrigger(notholdingHash);
+            anim.SetTrigger(holdingHash);
+        }
+        else if (distance_swap < .099f)
+        {
+            anim.ResetTrigger(notholdingHash);
+            anim.SetTrigger(holdingHash);
+        }
+        else
+        {
+            anim.ResetTrigger(holdingHash);
+            if (distance_speed > .1f)
+            {
+                anim.SetTrigger(notholdingHash);
+            }
+            else if (distance_swap > .1f)
+            {
+                anim.SetTrigger(notholdingHash);
+            }
+            else if (distance_flip > .1f)
+            {
+                anim.SetTrigger(notholdingHash);
+            }
         }
 
-	
-	}
+        
+        
+
+        
+        
+
+        
+              
+
+    }        
 }
